679319de6ea0187faad0a0f4ba7a38886cf45f1b
Ronald Rey <reyronald@gmail.com>
2020-12-22 19:25:02 -0400

    Add links

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 
 import { Map } from '../components/Map'
+import { WhatsAppIcon } from '../components/WhatsAppIcon'
+import { InstagramIcon } from '../components/InstagramIcon'
 
 import styles from '../styles/Home.module.css'
+import buttonStyles from '../styles/Button.module.css'
 
 export default function Home() {
   return (
@@ -35,6 +38,30 @@ export default function Home() {
             />{' '}
             <a href="https://instagram.com/petmansion">@petmansion</a>
           </p>
+
+          <div className={styles.links}>
+            <a
+              href="https://forms.gle/uDkVxnHQNnoygp5w8"
+              className={buttonStyles.button}
+            >
+              Reservas para nuevos clientes
+            </a>
+
+            <a
+              href="https://api.whatsapp.com/send?phone=18293057334"
+              className={buttonStyles.button}
+            >
+              <WhatsAppIcon style={{ marginRight: 7 }} /> WhatsApp
+              1-829-305-7334
+            </a>
+
+            <a
+              href="https://instagram.com/petmansion"
+              className={buttonStyles.button}
+            >
+              <InstagramIcon style={{ marginRight: 7 }} /> @petmansion
+            </a>
+          </div>
         </main>
       </div>
       <Map />
