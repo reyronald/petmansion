a1abaf7afdef8725d4b0aa038485de9d3076514c
Ronald Rey <reyronald@gmail.com>
2020-12-22 19:44:24 -0400

    Add links (#3)
    ![image iphone x](https://user-images.githubusercontent.com/7514993/102942513-f5358380-448b-11eb-8209-a104723f3a47.png)
    
    * Add links
    * Remove old instagram link
    * Fix document head warning
    * Add bottom padding to shorter screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 
 import '../styles/reset.css'
@@ -5,7 +7,25 @@ import '../styles/globals.css'
 
 // https://nextjs.org/docs/basic-features/typescript#custom-app
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My new cool app</title>
+
+        <title>Pet Mansion</title>
+
+        <meta httpEquiv="x-ua-compatible" content="ie=edge"></meta>
+        <meta name="application-name" content="Pet Mansion" />
+        <meta
+          name="description"
+          content="Hotel y peluquería para perros libre de jaulas en Santo Domingo, República Dominicana"
+        ></meta>
+
+        <link rel="icon" href="/branding/dog face small.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
