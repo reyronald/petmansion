961ffa2393c46233e46fb9f2d37e64fac6825401
Ronald Rey <reyronald@gmail.com>
2020-12-22 22:05:14 -0400

    more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,23 +21,21 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Hotel y peluquería para perros libre de jaulas en Santo Domingo, República Dominicana"
         ></meta>
 
-        <meta property="og:title" content="Pet Mansion"></meta>
+        <meta property="og:title" content="Pet Mansion" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://petmansion.com.do/" />
         <meta
           property="og:description"
           content="Hotel y peluquería para perros libre de jaulas"
-        ></meta>
+        />
         <meta
           property="og:image"
-          content="/branding/logo-petmansion.jpg"
-        ></meta>
-        <meta property="og:url" content="https://petmansion.com.do/"></meta>
-
-        <meta name="twitter:card" content="summary"></meta>
-        <meta
-          property="twitter:image"
           content="https://petmansion.com.do/branding/logo-petmansion.jpg"
-        ></meta>
-        <meta name="twitter:image:alt" content="Logo"></meta>
+        />
+        <meta name="og:image:alt" content="Logo" />
+        <meta property="og:locale" content="es_DO" />
+
+        <meta name="twitter:card" content="summary" />
 
         <link rel="icon" href="/branding/dog face small.ico" />
       </Head>
